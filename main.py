cf80867c7b06ea19ba71698aabb675f2557a17fd
Aten Chen <109785614+AtengChen@users.noreply.github.com>
2024-10-27 12:59:45 +1000

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+__import__('sys').exit(
+                  (_:=(lambda _builtins,math,time,os,itertools:[
+            (A:=0),(B:=0),_builtins.print('\x1b[2J',end=''),[[(z:=(
+        [0]*1760)),(b:=([' ']*1760)),[[[(c:=math.sin(i/100)),(d:=math.cos(
+      j/100)),(e:=math.sin(A)),(f:=math.sin(j/100)),(g:=math.cos(A)),(h:=(d+2
+     )),(D:=(1/(c*h*e+f*g+5))),(l:=math.cos(i/100)),(m:=math.cos(B)),(n:=math.sin(
+    B)),(t:=(c*h*g-f*e)),(        x:=_builtins.int(40+30*D*(l*h*m-t*n)
+   )),(y:=_builtins.int(               12+15*D*(l*h*n+t*m))),(o:=(x+80*y)
+  ),(N:=_builtins.int(                    8*((f*e-c*d*g)*m-c*d*e-f*g-l*d*n)
+  )),[z.__setitem__(o,                      D),b.__setitem__(o,'.,-~:;=!*#$@'[
+   N if N>0 else 0])]                      if 0<=y<22 and 0<=x<80 and(D>z[o])
+   else None]for i in                      _builtins.range(0,628,2)]for j in 
+   _builtins.range(0,                    628,7)],_builtins.print('\x1b[H'
+    ,end=''),[_builtins.               print(b[k],end='\n'if k%80==79 
+    else'')for k in _builtins.   range(1760)],(A:=(A+0.04)),(B:=
+        (B+0.02)),time.sleep(0.03)]for _ in itertools.count()],0]))(
+             __import__('builtins'),__import__('math'),__import__('time'),
+                __import__('os'),__import__('itertools'))[-1]) #--
+                 ##########################################
+                       ############################
+                         
